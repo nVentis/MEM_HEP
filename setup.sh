6c911db8f7a61619dd383200e0fe0ba6f6d20209
Bryan Bliewert <bryan.bliewert@nventis.eu>
2023-06-05 10:13:55 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ source /afs/desy.de/user/b/bliewert/public/MarlinWorkdirs/TestPhyssimK4H/only_ph
 
 echo "Using current directory as relative path for libraries, which is ${PWD}"
 
-export MARLIN_DLL=$MARLIN_DLL:$PWD/source/ZHHKinfitProcessors/lib/libZHHKinfitProcessors.so:$PWD/source/CompareTrueMEProcessor/lib/CompareTrueMEProcessor.so
+export MARLIN_DLL=$MARLIN_DLL:$PWD/source/ZHHPostRecoMEProcessor/lib/ZHHPostRecoMEProcessor.so:$PWD/source/CompareTrueMEProcessor/lib/CompareTrueMEProcessor.so
 
 export CMAKE_PREFIX_PATH=/afs/desy.de/user/b/bliewert/public/ILCSoft/Physsim:$CMAKE_PREFIX_PATH
 export LD_LIBRARY=$LCIO/lib64:$LD_LIBRARY_PATH
