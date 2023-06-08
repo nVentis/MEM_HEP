70bcc28ce41fce9fe6c29f2f324a2f67336ced39
Bryan Bliewert <bryan.bliewert@nventis.eu>
2023-06-09 00:42:38 +0200

    Prepare batch

--- jobs/2023-06-08_a/job.sh
+++ jobs/2023-06-08_a/job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7/v02-02-02/init_ilcsoft.sh &&
-source /afs/desy.de/user/d/bliewert/public/MarlinWorkdirs/setup.sh
+# source /cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7/v02-02-02/init_ilcsoft.sh &&
+source /afs/desy.de/user/d/bliewert/public/MarlinWorkdirs/MEM_HEP/setup.sh
 
 rm -rf job${2}
 mkdir job${2}
@@ -9,7 +9,7 @@ cd ./job${2}
 cp ${1} .
 filename=`ls *.slcio`
 
-Marlin /afs/desy.de/user/d/dudarboh/TOFAnalysis/xml/steer.xml --global.LCIOInputFiles="${filename}"
+Marlin /afs/desy.de/user/d/bliewert/public/MarlinWorkdirs/MEM_HEP/marlin_compare_true_me.xml --global.LCIOInputFiles="${filename}"
 mv TOFAnalysis_RENAME.root ${2}.root
 rm -f ../${2}.root
 mv ${2}.root ..
