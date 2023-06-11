9725b5292a43856d8e9f529994facdc7b6e568ac
nVentis <bryan.bliewert@nventis.eu>
2023-06-11 16:35:55 +0200

    Fixes on job

--- jobs/2023-06-08_a/job.sh
+++ jobs/2023-06-08_a/job.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # source /cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7/v02-02-02/init_ilcsoft.sh &&
-source /afs/desy.de/user/d/bliewert/public/MarlinWorkdirs/MEM_HEP/setup.sh
+source /afs/desy.de/user/b/bliewert/public/MarlinWorkdirs/MEM_HEP/setup.sh
 
 rm -rf job${2}
 mkdir job${2}
 cd ./job${2}
 
-cp ${1} .
-filename=`ls *.slcio`
-bname=`basename -s .slcio ./$filename`
+#ls -la
+#cp ${1} .
+#sleep 2
+#ls -la
+#filename=`ls *.slcio`
+filename=${1}
+bname=`basename -s .slcio $filename`
 
-Marlin /afs/desy.de/user/d/bliewert/public/MarlinWorkdirs/MEM_HEP/marlin_compare_true_me.xml --global.LCIOInputFiles="${filename}" --constant.OutputBaseName="${bname}"
-mv TOFAnalysis_RENAME.root ${2}.root
-rm -f ../${2}.root
-mv ${2}.root ..
+Marlin "/afs/desy.de/user/b/bliewert/public/MarlinWorkdirs/MEM_HEP/jobs/2023-06-08_a/marlin_file.xml" --global.LCIOInputFiles="${filename}" --constant.OutputBaseName="${bname}"
+mv *.root ../
+mv *.slcio ../
 
 cd ..
 rm -rf job${2}
--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ echo "Using current directory as relative path for libraries, which is ${PWD}"
 
 # Modules of ZHH
 export ILD_ANASOFT_ZHH=/afs/desy.de/user/b/bliewert/public/MarlinWorkdirs/ZHH
+export MEM_HEP=/afs/desy.de/user/b/bliewert/public/MarlinWorkdirs/MEM_HEP
 
 export MARLIN_DLL=$MARLIN_DLL:/afs/desy.de/user/b/bliewert/public/ILCSoft/Physsim/build/lib/libPhyssim.so
 export MARLIN_DLL=$MARLIN_DLL:$ILD_ANASOFT_ZHH/source/AddNeutralPFOCovMat/lib/libAddNeutralPFOCovMat.so
@@ -21,8 +22,8 @@ export MARLIN_DLL=$MARLIN_DLL:$ILD_ANASOFT_ZHH/source/ZHHKinfitProcessors/lib/li
 export MARLIN_DLL=$MARLIN_DLL:$ILD_ANASOFT_ZHH/source/Misclustering/lib/libMisclustering.so
 
 # Modules of MEM_HEP
-export MARLIN_DLL=$MARLIN_DLL:$PWD/source/ZHHPostRecoMEProcessor/lib/libZHHPostRecoMEProcessor.so
-export MARLIN_DLL=$MARLIN_DLL:$PWD/source/CompareTrueMEProcessor/lib/libCompareTrueMEProcessor.so
+export MARLIN_DLL=$MARLIN_DLL:$MEM_HEP/source/ZHHPostRecoMEProcessor/lib/libZHHPostRecoMEProcessor.so
+export MARLIN_DLL=$MARLIN_DLL:$MEM_HEP/source/CompareTrueMEProcessor/lib/libCompareTrueMEProcessor.so
 
 # Other modules
 export MARLIN_DLL=$MARLIN_DLL:/afs/desy.de/user/b/bliewert/public/yradkhorrami/SLDecayCorrection/build/lib/libSLDecayCorrection.so
