388f134fb9677473be4135f92d84538c406267a8
nVentis <bryan.bliewert@nventis.eu>
2023-10-05 13:46:06 +0200

    Updates

--- analysis/plot_root.py
+++ analysis/plot_root.py
@@ -1,10 +1,31 @@
 from sys import platform
+from typing import Optional
 import numpy as np
+import pandas as pd
 if platform == "linux" or platform == "linux2":
     import ROOT
     # %jsroot off
     
-def plot_hist(data, x, labels=None, colorpalette=None, bins=128, xlabel="", ylabel="", units="", normalize=False, title="Likelihood-Analysis"):
+def plot_hist(data:pd.DataFrame, x, labels:Optional[dict]=None, colorpalette=None, bins=128, xlabel:str="", ylabel:str="", xunits:str="", yunits:str="", normalize=False, title="Some Plot"):
+    """Plots one ore multiple histograms using ROOT
+
+    Args:
+        data (pd.DataFrame): dataframe, i.e. dict with of structure: keys als column names and lists as values
+        x (_type_): columns
+        labels (_type_, optional): _description_. Defaults to None.
+        colorpalette (_type_, optional): If None, defaults to a predefined array of colors. See https://root.cern.ch/doc/master/classTColor.html#C01. Defaults to None.
+        bins (int, optional): number of bins. Defaults to 128.
+        xlabel (str, optional): label for x axis. Defaults to "".
+        ylabel (str, optional): label for y axis. Defaults to "".
+        xunits (str, optional): units for x axis. Defaults to "".
+        yunits (str, optional): units for y axis. Defaults to "".
+        normalize (bool, optional): whether or not to normalize by function area. Defaults to False.
+        title (str, optional): _description_. Defaults to "Some Plot".
+
+    Returns:
+        _type_: _description_
+    """
+    
     # Get colorpalette
     pal = colorpalette if colorpalette is not None else [2,4,5,6,7,8,9,10,11]
 
@@ -24,11 +45,11 @@ def plot_hist(data, x, labels=None, colorpalette=None, bins=128, xlabel="", ylab
         hists.append(ROOT.TH1D(h_name, h_title, bins, g_min, g_max))
 
     xaxis = hists[0].GetXaxis()
-    xaxis.SetTitle(xlabel + (" [" + units + "]" if units != "" else ""))
+    xaxis.SetTitle(xlabel + (" [" + xunits + "]" if xunits != "" else ""))
     
     ylabel = "Normalized" if (normalize and ylabel == "") else ""
     yaxis = hists[0].GetYaxis()
-    yaxis.SetTitle(ylabel)
+    yaxis.SetTitle(ylabel + (" [" + yunits + "]" if yunits != "" else ""))
     
     #ROOT.gStyle.SetErrorX(0)
 
